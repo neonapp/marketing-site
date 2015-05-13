dfe3db23c6535add3a156a88fa1ae19ff93394bd
Diego Netto <diegormnetto@gmail.com>
2015-05-12 23:22:54 -0500

    Hooked up mailchimp and updated social media links

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  $('#signup-submit').click(function () {
+    var email = $('#signup-email').val();
+    var url = 'http://thelavaapp.com/subscribe?callback=?&email=' + email;
+    $.get(url, function (response) {
+      if (response.status === 'error') {
+        alert(response.error);
+      } else {
+        $('#signup-email').val('');
+        $('#form-wrapper').hide();
+        $('.success-message').show();
+      }
+    })
+    .fail(function(response) {
+      // TODO: Display error message
+      var msg = response.responseJSON ? response.responseJSON.error : 'Unable to reach server';
+      alert(msg);
+    });
+  });
+});
