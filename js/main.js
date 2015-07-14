f62e586795e1adf409026c7a3d08179c2f7aaaf5
Diego Netto <diegormnetto@gmail.com>
2015-07-14 00:49:02 -0500

    Wire up form

--- js/main.js
+++ js/main.js
@@ -6,6 +6,23 @@ function sizeHero() {
 }
 
 $(document).ready(function() {
+  $('#signup-submit').click(function () {
+    var email = $('#signup-email').val();
+    var url = '/subscribe?callback=?&email=' + email;
+    $.get(url, function (response) {
+      if (response.status === 'error') {
+        alert(response.error);
+      } else {
+        $('#signup-email').val('');
+        alert('Thank you for signing up!');
+      }
+    })
+    .fail(function(response) {
+      // TODO: Display error message
+      var msg = response.responseJSON ? response.responseJSON.error : 'Unable to reach server';
+      alert(msg);
+    });
+  });
   sizeHero();
 });
 
