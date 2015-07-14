356b5ac43d65b78d2bdc0dc0310f6ca5af92be8b
Diego Netto <diegormnetto@gmail.com>
2015-07-14 00:28:43 -0500

    New site

--- js/main.js
+++ js/main.js
@@ -1,20 +1,14 @@
+function sizeHero() {
+  var wh = $(window).height();
+  var ww = $(window).width();
+  $('.hero').height(wh);
+  $('.hero').width(ww);
+}
+
 $(document).ready(function() {
-  $('#signup-submit').click(function () {
-    var email = $('#signup-email').val();
-    var url = '/subscribe?callback=?&email=' + email;
-    $.get(url, function (response) {
-      if (response.status === 'error') {
-        alert(response.error);
-      } else {
-        $('#signup-email').val('');
-        $('#form-wrapper').hide();
-        $('.success-message').show();
-      }
-    })
-    .fail(function(response) {
-      // TODO: Display error message
-      var msg = response.responseJSON ? response.responseJSON.error : 'Unable to reach server';
-      alert(msg);
-    });
-  });
+  sizeHero();
+});
+
+$(window).resize(function() {
+  sizeHero();
 });
